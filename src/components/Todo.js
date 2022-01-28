0069c57bc1f13daa6ff64e0da8c08a36248a9fe2
Nimeshtank <93155144+Nimeshtank@users.noreply.github.com>
2022-01-28 19:32:20 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,33 @@
  import React from 'react';
 
  export const Todo  = () =>{
+     const [todo, setTodo] = useState([]);
+
+  async function fetchData() {
+    const data = await fetch("https://jsonplaceholder.typicode.com/todos ");
+    const res = await data.json();
+
+    setTodo(res);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
     
+      {todo.map((data) => {
+        return (
+
+         <div  className="todo" id="todo-${data.id}">
+
+           <h1>{data.title}</h1>
+           <h1>{data.completed ? 'Completed' : 'Incomplete'}</h1>
+         </div>
+         
+        );
+      })}
+    </>
+  );
  }

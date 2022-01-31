3ed904224cf873f02943beb09bae20d42dea7a81
Nimeshtank <93155144+Nimeshtank@users.noreply.github.com>
2022-01-31 19:37:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import '../styles/App.css';
-import { Loader } from './Loader';
 import { Todo } from './Todo';
+
 const App = () => {
   
   return (
     <div className="App">
-      <Loader />
       <Todo  />
     </div>
   );

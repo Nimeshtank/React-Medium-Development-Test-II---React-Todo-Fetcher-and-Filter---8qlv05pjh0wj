fb667bf98eee3dd08e3cfb9fe0ca3aeafacedb20
Nimeshtank <93155144+Nimeshtank@users.noreply.github.com>
2022-01-31 19:43:50 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,33 +1,88 @@
- import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Loader } from "./Loader";
+import TodoItems from "./TodoItems";
 
- export const Todo  = () =>{
-     const [todo, setTodo] = useState([]);
+export const Todo = () => {
+ 
+  const [todo, setTodo] = useState([]);
+  const[filterData, setFilterData] = useState([])
+  const [loading, setloading] = useState(false);
+  const [complete, setComplete] = useState(true);
+  const [inComplete, setInComplete] = useState(true);
+
 
   async function fetchData() {
+    setloading(true);
     const data = await fetch("https://jsonplaceholder.typicode.com/todos ");
-    const res = await data.json();
-
+    let res = await data.json();
+    res = res.splice(0, 20);
     setTodo(res);
+    setloading(false);
   }
 
   useEffect(() => {
     fetchData();
   }, []);
 
+
+  const compCheck = (type, e) => {
+    if(type === 'complete'){
+      setComplete(e.target.checked);
+   
+    }
+    else if(type === 'incomplete'){
+      setInComplete(e.target.checked);
+     
+    }
+  };
+
+  useEffect(()=>{
+    if(complete && inComplete){
+       setFilterData(todo);
+    }
+    if(complete && !inComplete){
+      setFilterData(todo.filter((data)=> data.completed))
+    }
+    if(inComplete && !complete){
+      setFilterData(todo.filter((data)=> !data.completed))
+    }
+    if(!complete && !inComplete){
+      setFilterData([]);
+    }
+  }, [inComplete, complete, todo])
+
   return (
     <>
-    
-      {todo.map((data) => {
-        return (
+      {loading && (
+        <h1>
+          <Loader />
+        </h1>
+      )}
+      {!loading && (
+        <>
+          <TodoItems todo={filterData} loading={loading} />
 
-         <div  className="todo" id="todo-${data.id}">
+          <div id="filter-holder">
+            <label>Show Completed</label>
+            <input
+              id="completed-checkbox"
+              type="checkbox"
+              onChange={(e)=>{compCheck('complete', e)}}
+              checked={complete}
+            />
+            <br />
 
-           <h1>{data.title}</h1>
-           <h1>{data.completed ? 'Completed' : 'Incomplete'}</h1>
-         </div>
-         
-        );
-      })}
+            <label>Show Incompleted</label>
+            <input
+              id="incompleted-checkbox"
+              type="checkbox"
+              onChange={(e) => {compCheck('incomplete', e)} }
+              checked={inComplete}
+            />
+          </div>
+        </>
+      )}
     </>
   );
- }
+};
+
